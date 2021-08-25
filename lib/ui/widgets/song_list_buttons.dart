38e3b83c1ed2e6dc56cde6b5b99226d2255cd2bd
diegoroyo <diego.roymen@gmail.com>
2021-08-25 21:15:56 +0200

    feat: add 'Download All' button in song_list_button to make all songs available for download

--- lib/ui/widgets/song_list_buttons.dart
+++ lib/ui/widgets/song_list_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:app/constants/dimensions.dart';
 import 'package:app/models/song.dart';
 import 'package:app/providers/audio_provider.dart';
+import 'package:app/providers/cache_provider.dart';
 import 'package:app/ui/widgets/full_width_primary_icon_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,34 +17,93 @@ enum SongListContext {
   other,
 }
 
-class SongListButtons extends StatelessWidget {
+class SongListButtons extends StatefulWidget {
   static final Key playAllButtonKey = UniqueKey();
   static final Key shuffleAllButtonKey = UniqueKey();
+  static final Key downloadAllButtonKey = UniqueKey();
 
   final List<Song> songs;
 
   const SongListButtons({Key? key, required this.songs}) : super(key: key);
 
   @override
+  _SongListButtonsState createState() => _SongListButtonsState();
+}
+
+class _SongListButtonsState extends State<SongListButtons> {
+  late CacheProvider cache;
+  bool _downloading = false;
+  int _numDownloadedSongs = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    cache = context.read();
+    _checkDownloadedSongs();
+  }
+
+  void _checkDownloadedSongs() async {
+    for (Song song in widget.songs) {
+      if (await cache.has(song: song)) {
+        _numDownloadedSongs++;
+      }
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     AudioProvider audio = context.read();
     return Container(
       padding: const EdgeInsets.all(AppDimensions.horizontalPadding),
-      child: Row(
-        children: <Widget>[
-          FullWidthPrimaryIconButton(
-            key: SongListButtons.playAllButtonKey,
-            icon: CupertinoIcons.play_fill,
-            label: 'Play All',
-            onPressed: () async => await audio.replaceQueue(songs),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            children: <Widget>[
+              FullWidthPrimaryIconButton(
+                key: SongListButtons.playAllButtonKey,
+                icon: CupertinoIcons.play_fill,
+                label: 'Play All',
+                onPressed: () async => await audio.replaceQueue(widget.songs),
+              ),
+              const SizedBox(width: 12),
+              FullWidthPrimaryIconButton(
+                key: SongListButtons.shuffleAllButtonKey,
+                icon: CupertinoIcons.shuffle,
+                label: 'Shuffle All',
+                onPressed: () async =>
+                    await audio.replaceQueue(widget.songs, shuffle: true),
+              ),
+            ],
           ),
-          const SizedBox(width: 12),
-          FullWidthPrimaryIconButton(
-            key: SongListButtons.shuffleAllButtonKey,
-            icon: CupertinoIcons.shuffle,
-            label: 'Shuffle All',
-            onPressed: () async =>
-                await audio.replaceQueue(songs, shuffle: true),
+          const SizedBox(height: 10),
+          Row(
+            children: <Widget>[
+              FullWidthPrimaryIconButton(
+                key: SongListButtons.downloadAllButtonKey,
+                icon: _numDownloadedSongs == widget.songs.length
+                    ? CupertinoIcons.checkmark_alt_circle_fill
+                    : CupertinoIcons.cloud_download_fill,
+                label: _numDownloadedSongs == widget.songs.length
+                    ? 'All songs available offline'
+                    : 'Download All'
+                        '${_downloading ? " (" + _numDownloadedSongs.toString() + " / " + widget.songs.length.toString() + ")" : ""}',
+                onPressed: () async {
+                  if (!_downloading) {
+                    setState(() => _downloading = true);
+                    for (Song song in widget.songs) {
+                      if (!await cache.has(song: song)) {
+                        await cache
+                            .cache(song: song)
+                            .then((e) => setState(() => _numDownloadedSongs++));
+                      }
+                    }
+                    setState(() => _downloading = false);
+                  }
+                },
+              ),
+            ],
           ),
         ],
       ),
